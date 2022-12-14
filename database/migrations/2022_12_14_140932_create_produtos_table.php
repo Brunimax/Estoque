c5856ddebefec189059accae6c2d793e175c45b6
bruno.medrado <bruno.medrado@saude.se.gov.br>
2022-12-14 11:18:20 -0300

    first commit

---
+++ database/migrations/2022_12_14_140932_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tipo_id');
+            $table->string('imagem', 100);
+            $table->timestamps();
+
+            $table->foreign('tipo_id')->references('id')->on('tipos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
